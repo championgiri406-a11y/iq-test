55dded295393f79116f9cf445ea9ad3f4cbc21ed
championgiri406-a11y <championgiri406@gmail.com>
2026-07-30 06:30:31 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,13 +1,14 @@
+let score = 0;
 
 const questions = [
-  {
-    question: "What number comes next?\n2, 4, 8, 16, ?",
-    options: ["18", "24", "32", "34"],
-    answer: 2
-  },
-  {
-    question: "Which shape has exactly 4 equal sides?",
-    options: ["Rectangle", "Square", "Triangle", "Circle"],
-    answer: 1
-  }
+{
+question:"What number comes next?\n2, 4, 8, 16, ?",
+options:["18","24","32","34"],
+answer:2
+},
+{
+question:"Which shape has exactly 4 equal sides?",
+options:["Rectangle","Square","Triangle","Circle"],
+answer:1
+}
 ];
