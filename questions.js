527f7c1bca76db4fb4eeda6e5f4a5b266e407be8
championgiri406-a11y <championgiri406@gmail.com>
2026-07-31 06:30:02 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,12 +1,38 @@
 const questions = [
+
 {
-question:"What number comes next?\n2, 4, 8, 16, ?",
-options:["18","24","32","34"],
+question:"Find the missing number:\n2, 6, 12, 20, 30, ?",
+options:["36","40","42","48"],
 answer:2
 },
+
 {
-question:"Which shape has exactly 4 equal sides?",
-options:["Rectangle","Square","Triangle","Circle"],
+question:"Which number does NOT belong?\n7, 14, 28, 54, 56",
+options:["14","28","54","56"],
+answer:2
+},
+
+{
+question:"If all BLOPS are RINS, and all RINS are TARS, which statement must be true?",
+options:[
+"All TARS are BLOPS",
+"Some TARS are BLOPS",
+"All BLOPS are TARS",
+"No BLOPS are TARS"
+],
+answer:2
+},
+
+{
+question:"A cube is painted on all six faces and then cut into 27 equal cubes. How many small cubes have exactly two painted faces?",
+options:["8","12","16","20"],
 answer:1
+},
+
+{
+question:"Find the missing number:\n1, 4, 9, 16, 25, ?",
+options:["30","35","36","49"],
+answer:2
 }
+
 ];
