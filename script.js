96b34d09e8966374e77e5fbd23555b867f8d11cd
championgiri406-a11y <championgiri406@gmail.com>
2026-07-31 16:33:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -177,7 +177,15 @@ iq >= 95 ? "🥉 Average" :
         ${score} / ${questions.length}
     </div>
 </div>
+<div class="score-box">
+    <h3>🏆 Best IQ</h3>
+    <div class="score-number">${bestIQ}</div>
+</div>
 
+<div class="score-box">
+    <h3>⭐ Best Score</h3>
+    <div class="score-number">${bestScore} / ${questions.length}</div>
+</div>
 <button class="next-btn" onclick="location.reload()">
 🔄 Restart Test
 </button>
